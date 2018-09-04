e94d15f36d1428eb45f56edb30711ccd1589b214
TimMueller <kvv1997@googlemail.com>
2018-09-04 16:44:04 +0200

    Added main street. Needs some adjustments regarding terrain

--- src/citygen.py
+++ src/citygen.py
@@ -1,13 +1,18 @@
 import pygame, sys
 import numpy as np
-import scipy
+import scipy as sci
+import os
+from scipy.interpolate import splprep, splev
 import time
 from random import randint
 from noise import snoise2, pnoise2, snoise3
 from pygame.locals import *
+import matplotlib.pyplot as plt
+import bezier
 
-
-TILESIZE = 8
+TILESIZE = 6
+WIDTH    = 110
+HEIGHT   = 110
 
 #constants representing colours
 RED   = (200,   0,   0  )
@@ -80,6 +85,18 @@ def clean_map(map):
                 else:
                     changed = False
 
+def fix_mainstreet(map):
+    for y in range(1,map.shape[0]-1):
+        for x in range(1,map.shape[1]-1):
+            # Check if street
+            if map[y][x] == STREET:
+                # check if the street is diagonal
+                if map[y-1][x-1] == STREET and map[y][x-1] != STREET:
+                    map[y-1][x] = STREET # Fill the diagonal
+                    #map[y-1][x+1] = GRASS# Create grass in the surrounding
+                elif map[y-1,x+1] == STREET and map[y][x+1] != STREET:
+                    map[y-1][x] = STREET
+
 # A quadratic matrix of size 2r+1  with a circle of radius r of ones around the center
 def circle_matrix(r):
     A = np.arange(-r,r+1)**2
@@ -99,7 +116,8 @@ def score_location(map, citysize, xpos, ypos):
     score = 0
     score += counter[GRASS]
     score -= counter[MOUNTAIN]
-    score += np.log(abs(counter[WATER]/(np.pi*citysize**2)-desired_water))
+    zielwasser = desired_water*np.pi*citysize**2
+    score += 2*(zielwasser-abs(counter[WATER]-zielwasser))
     return score
 
 # Assign a score value to each buildable tile (GRASS)
@@ -114,22 +132,47 @@ def score_locations(map,citysize):
                 scores[y][x] = 0
     return scores
 
+"""
+Erst straße random durch das Terrain legen und anhand dessen den Startpunkt wählen
+Oder erst Punkt wählen in den dann eine Straße von außen führt
+
+-> radius um mittelpnkt
+
+Stadtteilmittelpunkt wird vom Planer festgelegt
+->
+
+"""
+
 
 class Agent:
     def __init__(self, startx, starty):
         self.x = startx
         self.y = starty
+        self.type = 'agent'
+    # Intercat with the enviroment
+    def changeEnv(self, map):
+        pass
+    def __str__(self):
+        return self.type + ' at [' + str(self.x) + ', ' + str(self.y) + ']'
+
+class Explorer(Agent):
+    def __init__(self, startx, starty):
+        super().__init__(startx, starty)
+        self.type = 'explorer'
+    def changeEnv(self,map):
+        pass
 
 class StreetBuilder(Agent):
     def __init__(self, startx, starty):
-        agent.__init__(self, startx, starty)
+        super().__init__(startx, starty)
+        self.type = 'streetbuilder'
 
     def changeEnv(self, map):
         map[self.y][self.x] = STREET # build street
         nbh = get_neighbourhood(map, self.x, self.y)
         street_top   = False
         street_down  = False
-        street_left  = False
+        streetagents_left  = False
         street_right = False
         if map[self.y-1][self.x] == STREET:
             street_top = True
@@ -140,31 +183,64 @@ class StreetBuilder(Agent):
         if map[self.y][self.x+1] == STREET:
             street_right = True
 
-
-        direction = randint(1,2)
-        if direction == 1:
-            self.x += 1
+        direction = randint(1,10)
+        if direction <= 7:
+            self.x += randint(-1,1)
         else:
-            self.y += 1
+            self.y += randint(-1,1)
+
 
 octaves = 6
 freq = 10.0 * octaves
 
-map = generate_terrain(100,100)
-
-
+map = generate_terrain(WIDTH,HEIGHT)
 # NOTE: erode and dilate
 clean_map(map) # removes bad terrain
 
 scoremap = score_locations(map,15)
-maxel = np.unravel_index(np.argmax(scoremap, axis=None), scoremap.shape)
+plt.rcParams['figure.figsize'] = [10, 10]
+plt.matshow(scoremap, interpolation='nearest', cmap='jet')
+plt.colorbar()
+plt.show()
+
+max = np.unravel_index(np.argmax(scoremap, axis=None), scoremap.shape)
+citycenter = np.array([max[1],max[0]])
+print(max)
+
+# Generate random external 'mapentrance'
+connected_side = randint(1,4)
+if connected_side % 2 == 1:
+    extern = np.array([randint(0, WIDTH), 0])
+else:
+    extern = np.array([0, randint(0, HEIGHT)])
+
+# Create the route from the entrance to the citycentrum
+distance = np.linalg.norm(extern-np.array([citycenter[1], citycenter[0]]))
+sections = int(round(distance/10))
+# Create a point every ~10 tiles with a small random noise
+# These are the control points for a beziercurve fomring the street
+points = [extern]
+for i in range(1,sections):
+    points += [((1-i/sections)*extern+i/sections*citycenter).astype(int) + np.random.randint(-4,4,size=2)]
+points += [citycenter]
+
+# Create a beziercurve with points as control points
+curve = bezier.Curve(np.asfortranarray(points, dtype = float).T, degree=2)
+for i in range(2*int(curve.length)):
+    pt = curve.evaluate(i/(2*curve.length)).astype(int) # Evaluate gleichverteilt über die kurve
+    map[pt[0][0]][pt[1][0]] = STREET
+
+fix_mainstreet(map)
+
+agents = []
+#agents += [Explorer(citycenter[1], citycenter[0])]
+#agents += [StreetBuilder(citycenter[1], citycenter[0])]
+
+
+
 #set up the display
 pygame.init()
-
 DISPLAYSURF = pygame.display.set_mode((map.shape[1]*TILESIZE,map.shape[0]*TILESIZE))
-
-str = StreetBuilder(maxel[1], maxel[0])
-
 while True:
     #get all the user events
     for event in pygame.event.get():
@@ -172,19 +248,25 @@ while True:
         if event.type == QUIT:
             #and the game and close the window
             pygame.quit()
-            sys.exit()
+            os._exit(1)
 
     draw_map(map, DISPLAYSURF, TILESIZE)
     pygame.draw.rect(
             DISPLAYSURF,
             BLACK,
-            (maxel[1]*TILESIZE,maxel[0]*TILESIZE,TILESIZE,TILESIZE))
+            (citycenter[1]*TILESIZE,citycenter[0]*TILESIZE,TILESIZE,TILESIZE))
     pygame.draw.rect(
             DISPLAYSURF,
-            RED,
-            (str.x*TILESIZE,str.y*TILESIZE,TILESIZE,TILESIZE))
+            BLACK,
+            (extern[1]*TILESIZE,extern[0]*TILESIZE,TILESIZE,TILESIZE))
 
-    str.changeEnv(map)
+    for agent in agents:
+        agent.changeEnv(map)
+        pygame.draw.rect(
+                DISPLAYSURF,
+                RED,
+                (agent.x*TILESIZE,agent.y*TILESIZE,TILESIZE,TILESIZE))
+
     time.sleep(1)
     #update the display
     pygame.display.update()
